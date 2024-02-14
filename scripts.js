a99b862b2317a44ec3f96c135cb97c0a0f0ad9e8
Theo <sagniez.theo@laposte.net>
2024-02-14 10:54:42 +0100

    added functionality to click on word blocks to organise sentence

--- scripts.js
+++ scripts.js
@@ -1,15 +1,30 @@
-const wordBlocks = document.getElementsByClassName('wordBlock')
+/*
+
+Using Array.from() because:
+
+from --> https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection :
+
+An HTMLCollection in the HTML DOM is live; 
+it is automatically updated when the underlying document is changed. 
+For this reason it is a good idea to make a copy (e.g., using Array.from) 
+to iterate over if adding, moving, or removing nodes.
+
+*/
+
+const wordBlocks = Array.from(document.getElementsByClassName('wordBlock'))
 const buildSentence = document.querySelector('.buildSentence')
 
-// needs more work
 for (let i = 0; i < wordBlocks.length; i++) {
     wordBlocks[i].addEventListener('click', () => {
+
         let div = document.createElement('div')
         div.innerHTML = wordBlocks[i].innerHTML
-        div.classList.add('wordBlock')
+        div.classList.add('wordBlockClicked')
 
         wordBlocks[i].classList.add('hide')
+        wordBlocks[i].classList.remove('wordBlock')
 
         buildSentence.append(div)
     })
 }
+
