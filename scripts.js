697cc8e7bb88bab41b80ad66adfd98b30769687b
Theo <sagniez.theo@laposte.net>
2024-02-17 14:09:59 +0100

    worked on reset functionality - re-adding event listener does not work

--- scripts.js
+++ scripts.js
@@ -11,20 +11,46 @@ to iterate over if adding, moving, or removing nodes.
 
 */
 
-const wordBlocks = Array.from(document.getElementsByClassName('wordBlock'))
-const buildSentence = document.querySelector('.buildSentence')
-const wordBlocksContainer = document.querySelector('.wordBlocks')
+const removeChildren = container => {
+    if (container.childElementCount > 0) {
+        Array.from(container.children).forEach(el => container.removeChild(el))
+    }
+}
 
-for (let i = 0; i < wordBlocks.length; i++) {
-    wordBlocks[i].addEventListener('click', () => {
+const addEventToWordBlock = (wordBlock, wordBlocksContainer, buildSentence) => {
+    wordBlock.addEventListener('click', () => {
 
         let div = document.createElement('div')
-        div.innerHTML = wordBlocks[i].innerHTML
+        div.innerHTML = wordBlock.innerHTML
         div.classList.add('wordBlockClicked')
 
-        wordBlocksContainer.removeChild(wordBlocks[i])
+        wordBlocksContainer.removeChild(wordBlock)
 
         buildSentence.append(div)
     })
 }
 
+const wordBlocks = Array.from(document.getElementsByClassName('wordBlock'))
+const buildSentence = document.querySelector('.buildSentence')
+const wordBlocksContainer = document.querySelector('.wordBlocks')
+const reset = document.querySelector('.reset')
+
+for (let i = 0; i < wordBlocks.length; i++) {
+    addEventToWordBlock(wordBlocks[i], wordBlocksContainer, buildSentence)
+}
+
+reset.addEventListener('click', () => {
+    removeChildren(wordBlocksContainer)
+    removeChildren(buildSentence)
+
+    for (let i = 0; i < wordBlocks.length; i++) {
+        let div = document.createElement('div')
+        div.innerHTML = wordBlocks[i].innerHTML
+        div.classList.add(wordBlocks[i].className)
+
+        wordBlocksContainer.append(div)
+
+        // doesn't work -> event listener is not added
+        addEventToWordBlock(wordBlocks[i], wordBlocksContainer, buildSentence)
+    }
+})
